0a3e53a8bb26ab95ae571e60e4c9047e24c7dd72
Alcir Junior <junior@cajucomunica.com.br>
2022-06-22 13:22:19 -0300

    refactor: changing architecture

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,15 @@
+import { Response } from "express";
+
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+class ListCategoriesController {
+  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+
+  handle(response: Response): Response {
+    const categories = this.listCategoriesUseCase.execute();
+
+    return response.status(200).json(categories);
+  }
+}
+
+export { ListCategoriesController };
